f86f51257c1b183300841d5245863b8ed40f4853
tremmerich <timothyemmerich@gmail.com>
2018-01-01 18:00:11 -0700

    pg fixes

--- models/environments.rb
+++ models/environments.rb
@@ -4,6 +4,8 @@ configure :development do
 end
 
 configure :production do
-  set :database, 'postgresql:db/database.db'
+  configure :production do
+  set :database, {adapter: 'postgresql',  encoding: 'unicode', database: 'dcs55vldcse690', pool: 2, username: 'rhmddzqfnsizqt', password: '
+2235a02522f1bba39df217276873ce6a0c6b3786a11f356a768dfd0d953c9eae'}
   set :show_exceptions, true
 end
