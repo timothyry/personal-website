55fb08c776f00e6119058012924338c8abca65ac
tremmerich <timothyemmerich@gmail.com>
2018-01-01 17:35:52 -0700

    postgresql fixes

--- models/environments.rb
+++ models/environments.rb
@@ -2,3 +2,8 @@ configure :development do
   set :database, 'sqlite3:db/database.db'
   set :show_exceptions, true
 end
+
+configure :production do
+  set :database, 'postgresql:db/database.db'
+  set :show_exceptions, true
+end
